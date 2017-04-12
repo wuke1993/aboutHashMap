8abec87e39066498790148e74d47c3095f8f00bd
wuke1993 <2297184250@qq.com>
2017-04-12 11:06:49 +0800

    Test ConcurrentHashMap.

---
+++ src/aboutConcurrentHashMap/TestConcurrentHashMap.java
@@ -0,0 +1,40 @@
+package aboutConcurrentHashMap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author: wuke 
+ * @date  : 2017年4月12日 上午10:31:58
+ * Title  : TestConcurrentHashMap
+ * Description : 
+ */
+public class TestConcurrentHashMap {
+
+	private static ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<Integer, Integer>();
+	
+	public static void main(String[] args) {
+		new Thread("Thread1") {
+			@Override
+			public void run() {
+				map.put(3, 33);
+			}
+		};
+		
+		new Thread("Thread2") {
+			@Override
+			public void run() {
+				map.put(4, 44);
+			}
+		};
+		
+		new Thread("Thread3") {
+			@Override
+			public void run() {
+				map.put(7, 77);
+			}
+		};
+		
+		System.out.println(map);
+	}
+
+}
